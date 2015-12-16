1f47f81620cea1a1c9475d0c1f272f34a2d18609
Jian JIAO <mynetuser@outlook.com>
2015-12-16 22:41:05 +0800

    实现Google API保存账户信息
    - 使用谷歌同步保存
    * 在配置数据读出后，需要暂停一下，断点或弹窗，否则变量值无法使用。

--- js/fill_in_account_info.js
+++ js/fill_in_account_info.js
@@ -1,6 +1,12 @@
 ﻿function fill_in_account_info() {
-	frames["mainFrame"].document.getElementById("UserName1").value = "18100000000";
-	frames["mainFrame"].document.getElementById("PassWord1").value = "888888";
+	chrome.runtime.sendMessage(
+		'username', function(response) {
+			frames["mainFrame"].document.getElementById("UserName1").value = response;
+	});
+	chrome.runtime.sendMessage(
+		'password', function(response) {
+			frames["mainFrame"].document.getElementById("PassWord1").value = response;
+	});
 	frames["mainFrame"].document.getElementById("loginbutton1").click();
 }
 window.onload = fill_in_account_info;
---
+++ js/options.js
@@ -0,0 +1,20 @@
+﻿function display_settings() {
+	username = "";
+	chrome.storage.sync.get("username",function(object){
+		username = object['username'];
+	});
+	password = "";
+	chrome.storage.sync.get("password",function(object){
+		password = object['password'];
+	});
+	var testname = username;
+	alert('读取成功！');	//重要！去掉会导致配置文本框更新失败！
+	document.getElementById('UserName').value = username;
+	document.getElementById('Password').value = password;
+	document.getElementById('Save').onclick = function() {
+		chrome.storage.sync.set({'username':document.getElementById('UserName').value}, function(){});
+		chrome.storage.sync.set({'password':document.getElementById('Password').value}, function(){});
+		alert('保存成功！');
+	}
+}
+window.onload = display_settings;
