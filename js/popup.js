5e41076db5aad02fba5b708c94227c91f760c039
Jian JIAO <mynetuser@outlook.com>
2015-12-17 00:16:55 +0800

    添加点击图标显示帐号功能

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+﻿function display_account_info() {
+	var UserName = document.getElementById('UserName');
+	chrome.storage.sync.get("username",function(object){
+		UserName.value = object['username'];
+		});
+}
+window.onload = display_account_info;
