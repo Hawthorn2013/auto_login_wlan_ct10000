a5667b5b32a281c1b00e4be49f8e3d1d3e7a2860
Jian JIAO <mynetuser@outlook.com>
2015-12-16 18:22:08 +0800

    能够自动点击登录按钮
    - 不能使用onclick()方法，否则js出错，但console调试正常。

--- js/fill_in_account_info.js
+++ js/fill_in_account_info.js
@@ -1,5 +1,6 @@
 ﻿function fill_in_account_info() {
 	frames["mainFrame"].document.getElementById("UserName1").value = "18100000000";
 	frames["mainFrame"].document.getElementById("PassWord1").value = "888888";
+	frames["mainFrame"].document.getElementById("loginbutton1").click();
 }
 window.onload = fill_in_account_info;
