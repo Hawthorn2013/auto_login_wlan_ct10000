328ef7bc4b0ddddafcb81a323162c3beffe4431c
Jian JIAO <mynetuser@outlook.com>
2015-12-20 23:49:17 +0800

    重构代码
    - 使用局部变量代替全局变量
    - 移除不使用运行时访问设置的漏网之鱼

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,8 @@
 ﻿function display_account_info() {
-	UserName = document.getElementById('UserName');
-	EnableCycleLogin = document.getElementById('EnableCycleLogin');
-	LoginCycle = document.getElementById('LoginCycle');
-	chrome.storage.sync.get("username",function(object){
-		UserName.value = object['username'];
-	});
+	var UserName = document.getElementById('UserName');
+	var EnableCycleLogin = document.getElementById('EnableCycleLogin');
+	var LoginCycle = document.getElementById('LoginCycle');
+	var Login = document.getElementById('Login');
 	chrome.runtime.sendMessage({msg: 'msg_get_username'}, function(res) {
 		UserName.value = res.data;
 	});
@@ -14,17 +12,17 @@
 	chrome.runtime.sendMessage({msg: 'msg_get_login_cycle', data: LoginCycle.value}, function(res) {
 		LoginCycle.value = res.data;
 	});
+	Login.onclick = function() {
+		chrome.tabs.create({url: "http://www.baidu.com/"}, function(){});
+	};
+	EnableCycleLogin.onclick = function() {
+		chrome.runtime.sendMessage({msg: 'msg_set_cycle_login_flag', data: EnableCycleLogin.checked});
+	};
+	LoginCycle.onchange = function() {
+		if (!isNaN(parseInt(LoginCycle))) {
+			LoginCycle.value = 10;
+		}
+		chrome.runtime.sendMessage({msg: 'msg_set_login_cycle', data: LoginCycle.value});
+	};
 }
 window.onload = display_account_info;
-document.getElementById('Login').onclick = function() {
-	chrome.tabs.create({url: "http://www.baidu.com/"}, function(){});
-};
-document.getElementById('EnableCycleLogin').onclick = function() {
-	chrome.runtime.sendMessage({msg: 'msg_set_cycle_login_flag', data: EnableCycleLogin.checked});
-};
-document.getElementById('LoginCycle').onchange = function() {
-	if (!isNaN(parseInt(LoginCycle))) {
-		LoginCycle.value = 10;
-	}
-	chrome.runtime.sendMessage({msg: 'msg_set_login_cycle', data: LoginCycle.value});
-};
