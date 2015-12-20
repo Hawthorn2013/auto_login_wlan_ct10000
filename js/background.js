75d2287208649fb88e4557d92b354a5a8df0eb8a
Jian JIAO <mynetuser@outlook.com>
2015-12-20 20:37:53 +0800

    添加周期登录功能
    - 每分钟打开百度试图跳转到登陆界面登录
    * 应添加周期长度选项
    * 应添加断网检测功能

---
+++ js/background.js
@@ -0,0 +1,9 @@
+﻿function create_page_for_login() {
+	chrome.tabs.create({url: "http://www.baidu.com/"}, function(){});
+}
+
+chrome.alarms.create("cycle_login", {delayInMinutes: 1, periodInMinutes: 1});
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+	create_page_for_login();
+});
